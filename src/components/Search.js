d5ba12351cb20bab661e09758f39f3edffc91055
DaliaDandashi <78696190+DaliaDandashi@users.noreply.github.com>
2021-03-22 12:55:40 +0200

    Weather API

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import clear from "../img/weather-icons/clear.svg";
+//import clear from "../img/weather-icons/clear.svg";
 import "./Search.css";
 class Search extends React.Component {
   state = {
@@ -9,22 +9,22 @@ class Search extends React.Component {
 
   render() {
     return (
-      <div>
-        {this.state.input}
+      <div className="header">
+        <form onSubmit={this.props.getData}>
         <input
           type="text"
           id="input-name"
-          onChange={event => {
-            this.setState({ input: event.target.value });
-          }}
+          name="city"
+          
         />
         <button
           onClick={event => {
             this.props.handleInput(this.state.input);
           }}
         >
-          Say Hello
+          FIND WEATHER
         </button>
+        </form>
       </div>
     );
   }
--- src/components/WeatherItem.js
+++ src/components/WeatherItem.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
+import mostlycloudy from "./../img/weather-icons/mostlycloudy.svg";
 import { render } from "react-dom";
+import fakeWeatherData from "../fakeWeatherData.json";
 
-export default class WeatherItem extends Component {
-  render() {
-    return <div>Heello</div>;
-  }
-}
-export class SayHello extends Component {
-  render() {
+
+
+import "./../App.css";
+
+export default function  WeatherNow (props)
+{
+const {temp_min,temp_max,humidity,pressure,id,description}=props.api;
+  
+  console.log(id);
+    
     return (
-      <div style={{ color: this.props.color, backgroundColor: "yellow" }}>
-        Hello {this.props.name}
-      </div>
-    );
+      <main className="content">
+            <div> 
+            {props.getIcon(id)}
+            <div>
+           {description}</div>
+            </div>
+            <div>
+             <span><strong>Temperature </strong></span> 
+           {temp_min} &#176;C to {temp_max}  &#176;C
+            </div>
+            <ul>
+              <li><strong>Humidity </strong></li>
+              <li> {humidity}%</li>
+              <li><strong>Pressure</strong></li>
+              <li> {pressure}</li>
+            </ul>
+            
+           </main>
+    )
   }
-}
